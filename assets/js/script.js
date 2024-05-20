19b0de356d52a8820ce5e6d4f08279f679bcf8a6
afzalpatwary <afzalhp22@gmail.com>
2024-05-20 22:57:20 +0600

    wsent home page convert done

---
+++ assets/js/script.js
@@ -0,0 +1,76 @@
+/**
+ * @Script js for (Template/Project Name)
+ *
+ * @project     - Project Name
+ * @author      - 
+ * @created_by  - 
+ * @created_at  - 
+ * @modified_by -
+ */
+
+
+/**
+ * ========================================================
+ * this function execute when window properly loaded
+ * ===========================================================
+ */
+
+$(window).on('load', function () {
+
+    // code should be execute here
+
+});
+
+
+
+/**
+ * ========================================================
+ * this function execute when DOM element ready 
+ * ===========================================================
+ */
+
+$(document).ready(function () {
+
+    // testimonial-active
+    $('.testimonial-slide').slick({
+        dots: true,
+        infinite: false,
+        speed: 1500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplaySpeed: 1500,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+         
+        ]
+      });
+
+
+    
+
+
+});
+
+AOS.init();
